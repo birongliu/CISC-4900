6bb6e053f61e6bfa75f2c2e5e85cbce7ffba3ca2
birongliu <birongliu2003@gmail.com>
2024-09-01 15:56:08 -0400

    chore: Add Next.js configuration files and global styles

---
+++ frontend/src/app/ui/navigation/Header.tsx
@@ -0,0 +1,46 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+const navigationItems = ["About", "Feature", "Pricing"];
+
+export default function Header() {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <div
+      className={`flex justify-between items-center md:mb-auto ${
+        isOpen ? "mb-32" : ""
+      }`}
+    >
+      <Image
+        src="/logo.svg"
+        alt="pet selector logo"
+        className="w-24 h-24"
+        width={133}
+        height={96}
+      />
+      <ul
+        className={`items-center absolute  w-full md:relative md:justify-center md:top-auto top-20 flex-col flex md:w-auto ${
+          isOpen ? "block" : "hidden"
+        } md:flex md:flex-row `}
+      >
+        {navigationItems.map((item, index) => (
+          <li
+            className="w-full px-10 py-5 hover:bg-slate-300 md:hover:bg-in"
+            key={index}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      <div className="flex gap-1 px-5 md:px-0">
+        <button className="mr-5 bg-slate-300 p-3 md:block rounded">
+          WaitList
+        </button>
+        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <Image alt="menu" src={"/icons/close.svg"} height={43} width={50} /> : <Image alt="menu" src={"/icons/Menu.svg"} height={43} width={51} />}
+        </button>
+      </div>
+    </div>
+  );
+}
