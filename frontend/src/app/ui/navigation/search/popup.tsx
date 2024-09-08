87e2bf84d986485af530204536f9ad4e70b37309
birongliu <birongliu2003@gmail.com>
2024-09-08 16:51:15 -0400

    Refactor search popup component to display "No suggestions found" when there are no filtered items

--- frontend/src/app/ui/navigation/search/popup.tsx
+++ frontend/src/app/ui/navigation/search/popup.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Pet } from "@/app/utils/interface";
+import Link from "next/link";
 
 export default function PopOut({
   items,
+  handleClose,
 }: {
   items: Pet[];
   handleClose: () => void;
@@ -12,7 +14,7 @@ export default function PopOut({
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(search === "") {
+    if (search === "") {
       setFilteredItems(items);
       return;
     }
@@ -20,17 +22,25 @@ export default function PopOut({
       item.name.toLowerCase().includes(search.toLowerCase())
     );
     setFilteredItems(filtered);
+  };
+  
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      handleClose();
+    }
   }
 
   return (
     <div
       role="dialog"
-      className="fixed flex py-32 items-center flex-col inset-0 z-50 backdrop-blur-md ease-in animate-fade"
+      onClick={handleClick}
+      className="fixed flex py-32 items-center flex-col inset-0 z-50 backdrop-blur-md ease-in animate-fadeIn"
     >
       <form id="popup" onSubmit={handleSubmit} className="w-96 md:w-6/12">
         <input
           type="text"
           form="popup"
+          autoFocus={true}
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           className="relative h-12 rounded-md px-3 w-full bg-white text-black "
@@ -40,16 +50,26 @@ export default function PopOut({
       <ul
         className={`w-96 md:w-6/12 mt-2 overflow-auto px-2 rounded-md scroll-my-40 scroll-ml-6 h-52 dark:text-black bg-white`}
       >
-        <div className={`px-2 py-2 text-sm font-bold border-b-2 ${filteredItems.length > 0 ? "block" : "hidden"}`}>Suggestion</div>
-        {(filteredItems.length > 0) ? (
+        <div
+          className={`px-2 py-2 text-sm font-bold border-b-2 ${
+            filteredItems.length > 0 ? "block" : "hidden"
+          }`}
+        >
+          Suggestion
+        </div>
+        {filteredItems.length > 0 ? (
           (filteredItems || items).map((item, index) => (
             <li key={index} className="px-2 mt-1">
-              {item.name} {item.breed}
+              <Link href={"https://google.com"}>{item.name} {item.breed}</Link>
             </li>
           ))
         ) : (
-          <div className={`flex items-center font-semibold mt-1 h-40 justify-center`}>
-          {filteredItems.length > 0 ? "No suggestions" : "No suggestions found"}
+          <div
+            className={`flex items-center font-semibold mt-1 h-40 justify-center`}
+          >
+            {filteredItems.length > 0
+              ? "No suggestions"
+              : "No suggestions found"}
           </div>
         )}
       </ul>
