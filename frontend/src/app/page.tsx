ad832fc8b3f245eb77eb2e6e9aa2233362a15144
birongliu <birongliu2003@gmail.com>
2024-09-19 23:58:33 -0400

    Refactor code to remove unused import and console.log statement

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "./ui/landing/Hero";
 import About from "./ui/landing/About";
 import Explore from "./ui/landing/Explore";
 import Team from "./ui/landing/Team";
-import AnnouncementBanner from "./ui/shared/AnnouncementBanner";
 
 export default function Home() {
   return (
