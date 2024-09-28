d056cd57047ae9a1db41443fd81a2f8a43ec5f6b
birongliu <birongliu2003@gmail.com>
2024-09-28 15:36:40 -0400

    Refactor UI for 404 page and improve user experience

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,9 +1,16 @@
+"use client"
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function notFound() {
   return (
-    <div className="grid place-content-center min-h-svh">
-        <h1>404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
+    <div className="min-h-svh flex justify-center items-center flex-col gap-2">
+        <h1 className="text-7xl font-poppins font-bold">404</h1>
+        <h2 className="text-2xl font-poppins font-semibold">Sorry, we couldn&apos;t find this page.</h2>
+        <p className="text-center font-poppins">But don&apos;t worry, you can find plenty of other things on our homepage.</p>
+        <Link href="/"> 
+        <button className="border p-2 rounded hover:bg-blue-400">Back to homepage</button>
+        </Link>
     </div>
   )
 }
